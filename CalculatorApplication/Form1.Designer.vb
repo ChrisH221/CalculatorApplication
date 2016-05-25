af0fc95c59ac23fa3d308ad65e73af1d1a706d74
Chris <C.Howell2@uni.brighton.ac.uk>
2016-05-25 09:38:33 +0100

    Calculator Application Commit.
    My first application written in VB.

---
+++ CalculatorApplication/Form1.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(125, 46)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(48, 38)
+        Me.Button7.TabIndex = 0
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(179, 46)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(45, 38)
+        Me.Button8.TabIndex = 1
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(230, 46)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(45, 38)
+        Me.Button9.TabIndex = 2
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(125, 90)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(45, 38)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(179, 90)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(45, 38)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(230, 90)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(45, 38)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(125, 134)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(45, 38)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(179, 134)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(45, 38)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(230, 134)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(45, 38)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(179, 178)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(98, 38)
+        Me.Button10.TabIndex = 17
+        Me.Button10.Text = "="
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(128, 178)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(45, 38)
+        Me.Button12.TabIndex = 15
+        Me.Button12.Text = "0"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(74, 90)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(45, 38)
+        Me.Button13.TabIndex = 14
+        Me.Button13.Text = "+"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(23, 46)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(45, 38)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "x"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(23, 90)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(45, 38)
+        Me.Button16.TabIndex = 11
+        Me.Button16.Text = "-"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(74, 46)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(45, 38)
+        Me.Button17.TabIndex = 10
+        Me.Button17.Text = "÷"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(23, 178)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(96, 38)
+        Me.Button18.TabIndex = 9
+        Me.Button18.Text = "AC"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.RichTextBox1.Location = New System.Drawing.Point(23, 12)
+        Me.RichTextBox1.MaxLength = 20
+        Me.RichTextBox1.Multiline = False
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.RichTextBox1.Size = New System.Drawing.Size(252, 28)
+        Me.RichTextBox1.TabIndex = 20
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.WordWrap = False
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(23, 134)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(45, 38)
+        Me.Button11.TabIndex = 21
+        Me.Button11.Text = "√"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(74, 134)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(45, 38)
+        Me.Button15.TabIndex = 22
+        Me.Button15.Text = ")"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(289, 233)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+
+End Class
---
+++ CalculatorApplication/Form1.vb
@@ -0,0 +1,271 @@
+﻿Public Class Form1
+
+    'Strings for representing the two input numbers
+    Dim Number1 As String = ""
+    Dim Number2 As String = ""
+
+    Dim squareSymbol As String = ""
+
+    Dim symbol As String ' The operator being applied to the two inputs
+    Dim nextNum As Boolean ' Set equal to True when first input is completed and operator has been selected
+    Dim sum As Boolean
+
+    Dim square1 As Boolean
+    Dim square2 As Boolean
+
+    'Setup for assigning a function to each button that represents a number
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim button As Button() = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
+        For i As Integer = 0 To button.Length - 1
+            AddHandler button(i).Click, AddressOf numberPress
+        Next
+
+
+        MyBase.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        MyBase.MinimizeBox = False
+        MyBase.MaximizeBox = False
+
+
+    End Sub
+
+    'Logic for the button OnClick event
+    Function numberPress(ByVal sender As Object, ByVal e As EventArgs)
+        Dim button As Button
+        button = sender
+        If RichTextBox1.Text = "Error" Then RichTextBox1.Text = ""
+        If sum Then RichTextBox1.Text = ClearAll()
+        If nextNum Then
+            Number2 = Number2 + button.Text
+            If square1 And square2 Then
+
+                RichTextBox1.Text = Number2 + squareSymbol + symbol + "(" + Number1 + squareSymbol
+
+            ElseIf square1 Then
+
+                RichTextBox1.Text = Number2 + symbol + "(" + Number1 + squareSymbol
+
+
+            ElseIf square2 Then
+
+                RichTextBox1.Text = Number2 + squareSymbol + symbol + Number1
+
+            Else
+
+                RichTextBox1.Text = Number2 + symbol + Number1
+            End If
+
+        Else
+            Number1 = Number1 + button.Text
+            RichTextBox1.Text = Number1 + squareSymbol
+        End If
+    End Function
+
+    'Slightly different logic required for the button zero OnClick event
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        If RichTextBox1.Text = "Error" Then RichTextBox1.Text = ""
+        If sum Then RichTextBox1.Text = ClearAll()
+        If nextNum Then
+            If Number2.Length > 0 Then
+                Number2 = Number2 + "0"
+                RichTextBox1.Text = Number2 + symbol + Number1
+            End If
+        Else
+            If Number1.Length > 0 Then
+                Number1 = Number1 + "0"
+                RichTextBox1.Text = RichTextBox1.Text + "0"
+            End If
+
+        End If
+    End Sub
+
+    'A Function for resetting the state of global variables
+    Function ClearAll()
+        RichTextBox1.Text = ""
+        Number1 = ""
+        Number2 = ""
+        nextNum = False
+        symbol = ""
+        squareSymbol = ""
+        sum = False
+        square1 = False
+        square2 = False
+
+    End Function
+
+
+    'Returns all variables to original state
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        ClearAll()
+    End Sub
+
+    'Button_Click for addition the divide symbol to the current number and setting nextNum equal to True
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If sum Then RichTextBox1.Text = ClearAll()
+        If Number1.Length < 1 Then
+            RichTextBox1.Text = "Error"
+        Else
+            nextNum = True
+            symbol = " + "
+            If square1 Then
+                RichTextBox1.Text = symbol + "(" + Number1 + squareSymbol
+            Else
+                RichTextBox1.Text = symbol + Number1
+            End If
+
+        End If
+
+    End Sub
+
+
+    'Button_Click for appending the minus symbol to the current number and setting nextNum equal to True
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        If sum Then RichTextBox1.Text = ClearAll()
+        If Number1.Length < 1 Then
+            RichTextBox1.Text = "Error"
+        Else
+            nextNum = True
+            symbol = " - "
+            If square1 Then
+                RichTextBox1.Text = symbol + "(" + Number1 + squareSymbol
+            Else
+                RichTextBox1.Text = symbol + Number1
+            End If
+
+        End If
+
+    End Sub
+    'Button_Click for appending the multiplcation symbol to the current number and setting nextNum equal to True
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If sum Then RichTextBox1.Text = ClearAll()
+        If Number1.Length < 1 Then
+            RichTextBox1.Text = "Error"
+        Else
+            nextNum = True
+            symbol = " × "
+            If square1 Then
+                RichTextBox1.Text = symbol + "(" + Number1 + squareSymbol
+            Else
+                RichTextBox1.Text = symbol + Number1
+            End If
+
+        End If
+
+    End Sub
+
+    'Button_Click for appending the divide symbol to the current number and setting nextNum equal to True
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If sum Then RichTextBox1.Text = ClearAll()
+        If Number1.Length < 1 Then
+            RichTextBox1.Text = "Error"
+        Else
+            nextNum = True
+            symbol = " ÷ "
+            If square1 Then
+                RichTextBox1.Text = symbol + "(" + Number1 + squareSymbol
+            Else
+                RichTextBox1.Text = symbol + Number1
+            End If
+
+        End If
+
+    End Sub
+
+
+    'Equals button features a Select Case for selecting which operation to apply to the inputs
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim Num1 As Double
+        Dim Num2 As Double
+        Int32.TryParse(Number1, Num1)
+        Int32.TryParse(Number2, Num2)
+        If square1 Then
+            Num1 = Math.Sqrt(Num1)
+        End If
+        If square2 Then
+            Num2 = Math.Sqrt(Num2)
+        End If
+        If Number2.Length >= 1 Then
+            Select Case symbol
+                Case " + "
+                    RichTextBox1.Text = Num1 + Num2
+                Case " - "
+                    RichTextBox1.Text = Num1 - Num2
+                Case " ÷ "
+                    RichTextBox1.Text = Num1 / Num2
+                Case " × "
+                    RichTextBox1.Text = Num1 * Num2
+            End Select
+        Else
+            RichTextBox1.Text = "Error"
+        End If
+
+        'Manually reset some variables to original state
+        Number1 = ""
+        Number2 = ""
+        nextNum = False
+        symbol = ""
+        squareSymbol = ""
+        sum = False
+        square1 = False
+        square2 = False
+
+    End Sub
+
+
+    'Appends the square root operator to the appropriate input and flags if the operator is applied to either the first or second input
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+
+        squareSymbol = ")√"
+
+        If RichTextBox1.Text = "Error" Then
+            If Number1.Length < 1 Then
+                square1 = True
+                RichTextBox1.Text = Number1 + squareSymbol
+
+            End If
+
+        Else
+
+            If RichTextBox1.Text = "" Then
+                If Number1.Length < 1 Then
+                    square1 = True
+                    RichTextBox1.Text = Number1 + squareSymbol
+
+                End If
+            Else
+                If nextNum Then
+                    If square1 Then
+                        If Number2.Length < 1 Then
+                            square2 = True
+                            RichTextBox1.Text = Number2 + squareSymbol + symbol + "(" + Number1 + squareSymbol
+
+                        End If
+                    Else
+                        square2 = True
+                        RichTextBox1.Text = Number2 + squareSymbol + symbol + Number1
+                    End If
+
+                Else
+                    RichTextBox1.Text = "Error"
+                    Number1 = ""
+                    Number2 = ""
+                    squareSymbol = ""
+                End If
+
+            End If
+        End If
+    End Sub
+    'Append the closing brace to specify the number that the square root operation is applied to
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        If nextNum Then
+            Dim button As Button
+            button = Button10
+            button.PerformClick() 'If the square root is calulated for the second input, then call the equals method
+        Else
+            If square1 Then
+                RichTextBox1.Text = "(" + Number1 + squareSymbol
+            End If
+        End If
+
+    End Sub
+
+End Class
